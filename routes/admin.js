096c4d2c7c24cc22fd883887b95ef6f7b65f5d3b
Nick Hughes <nick@nickhughes.io>
2022-04-08 22:16:43 +1000

    Section 5 Complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 
 router.get('/add-product',(req, res, next) => {  
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Products</button></form>');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
 
 module.exports = router;
+  
